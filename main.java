81707afc3b5e80cf984b62debcd0a94225b57b47
Joaquin3010 <joaquin.sanchezmz@udlap.mx>
2025-04-24 16:39:03 -0600

    se corriguio el main el cual es donde se ejecutan todas las acciones del sistema, como la creacion de tareas, el ordenamiento de estas y la notificacion de cambio de tareas, este nos demuestra el funcionamiento de patrones de diseño como el patron observador y patron estrategia

--- main.java
+++ main.java
@@ -1,22 +1,42 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         GestorDeTareas gestor = new GestorDeTareas();
-        
-        gestor.agregarTarea(new Tarea("Entregar reporte", 3));
-        gestor.agregarTarea(new Tarea("Hacer examen", 1));
-        gestor.agregarTarea(new Tarea("Estudiar patrones", 2));
- 
- 
+
+        // Observador
+        UsuarioObservador usuario1 = new UsuarioObservador("Carlos");
+
+        // Tareas
+        Tarea t1 = new Tarea("Entregar reporte", 3);
+        Tarea t2 = new Tarea("Hacer examen", 1);
+        Tarea t3 = new Tarea("Estudiar patrones", 2);
+
+        // Asociar observador a cada tarea
+        t1.agregarObservador(usuario1);
+        t2.agregarObservador(usuario1);
+        t3.agregarObservador(usuario1);
+
+        // Agregar tareas al gestor
+        gestor.agregarTarea(t1);
+        gestor.agregarTarea(t2);
+        gestor.agregarTarea(t3);
+
+        // Mostrar tareas sin ordenar
         System.out.println("Tareas antes de ordenar:");
         gestor.mostrarTareas();
- 
- 
+
+        // Ordenar por prioridad
         gestor.setEstrategia(new OrdenarPorPrioridad());
         gestor.ordenarTareas();
- 
- 
-        System.out.println("\nTareas después de ordenar por prioridad:");
+        System.out.println("\nTareas ordenadas por prioridad:");
         gestor.mostrarTareas();
-    }
-}
+
+        // Cambiar estado de una tarea
+        t1.setEstado("En progreso");
+        t2.setEstado("Completada");
+
+        // Ordenar por estado
+        gestor.setEstrategia(new OrdenarPorEstado());
+        gestor.ordenarTareas();
+        System.out.println("\nTareas ordenadas por estado:");
+        gestor.mostrarTareas();
 
